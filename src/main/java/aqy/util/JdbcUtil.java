60b8cce3a47d38b98ce9c204aba039622e544c5a
dzyse <1029605368@qq.com>
2018-08-27 11:12:43 +0800

    登录功能

--- src/main/java/aqy/util/JdbcUtil.java
+++ src/main/java/aqy/util/JdbcUtil.java
@@ -4,9 +4,9 @@ import java.sql.*;
 
 public class JdbcUtil {
    public static String driver="com.mysql.jdbc.Driver";
-   public static String url="jdbc:mysql://localhost:3306/aiqiyi";
+   public static String url="jdbc:mysql://localhost:3306/aqy";
    public static String username="root";
-   public static String password="12345";
+   public static String password="13866950037";
    static {
        try {
            Class.forName(driver);
