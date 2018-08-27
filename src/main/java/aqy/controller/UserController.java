99d2edc06fe9dd9a27dbfefbd02674ec50b89ca9
dzyse <1029605368@qq.com>
2018-08-27 14:58:41 +0800

    登录功能

--- src/main/java/aqy/controller/UserController.java
+++ src/main/java/aqy/controller/UserController.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 
 public class UserController {
     public void login(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        System.out.println("进入Controller层方法");
         String cardno = request.getParameter("cardno");
         String password =request.getParameter("password");
         AccountService accountService=new AccountService();
@@ -17,9 +18,11 @@ public class UserController {
             Account account=accountService.login(cardno,password);
             request.getSession().setAttribute("user",account);
             String JSONStr=JSON.toJSONString(account);
+            System.out.println(JSONStr);
             response.getWriter().write(JSONStr);
         } catch (Exception e) {
             response.getWriter().write(JSON.toJSONString(e.getMessage()));
+            System.out.println(JSON.toJSONString(e.getMessage()));
         }
     }
 }
