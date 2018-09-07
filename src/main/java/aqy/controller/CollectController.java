a75c803bc8b07896a016240c6a1975d0c368f5e0
kjc2 <1213367482@qq.com>
2018-09-07 12:08:19 +0800

    取点赞数

--- src/main/java/aqy/controller/CollectController.java
+++ src/main/java/aqy/controller/CollectController.java
@@ -3,6 +3,7 @@ package aqy.controller;
 import aqy.service.CollectService;
 import com.alibaba.fastjson.JSON;
 
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
